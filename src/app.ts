2b809a235d2617eba1f0517c282d92ae5df9bdfb
prodipkumarshil <prodipkrishna01@gmail.com>
2026-08-18 12:20:47 +0600

    added not found middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { type Application, type Request, type Response } from 'express'
 import cookieParser from 'cookie-parser'
+import { notFoundHandler } from './middleware/not-found'
 
 const app: Application = express()
 
@@ -9,5 +10,7 @@ app.use(cookieParser())
 app.get('/', (req: Request, res: Response) => {
   res.send("Server is running")
 })
+
+app.use(notFoundHandler)
 
 export default app
