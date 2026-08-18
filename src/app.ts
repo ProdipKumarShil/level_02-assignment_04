6b40b411acf36d5d97c22ae3020c66345ea57a78
prodipkumarshil <prodipkrishna01@gmail.com>
2026-08-19 01:07:03 +0600

    added deployment things

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { type Application, type Request, type Response } from 'express'
 import cookieParser from 'cookie-parser'
 import { notFoundHandler } from './middleware/not-found'
+import prisma from './lib/prisma'
 import authRouter from './modules/auth/auth.routes'
 import { globalErrorHandle } from './middleware/global-error'
 
@@ -11,6 +12,15 @@ app.use(cookieParser())
 
 app.get('/', (req, res) => {
   res.send("Server is running")
+})
+
+app.get('/api/user', async (req: Request, res: Response) => {
+  try {
+    const users = await prisma.user.findMany()
+    res.json(users)
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 app.use('/api/auth', authRouter)
