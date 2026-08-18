43749b1d0dc61d31ab18601feae0359a5681ea58
prodipkumarshil <prodipkrishna01@gmail.com>
2026-08-18 16:18:56 +0600

    added prisma

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { type Application, type Request, type Response } from 'express'
 import cookieParser from 'cookie-parser'
 import { notFoundHandler } from './middleware/not-found'
+import prisma from './lib/prisma'
 
 const app: Application = express()
 
---
+++ src/modules/technician/technician.routes.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const technicianRouter = Router()
+
+technicianRouter.get('/')
+
+export default technicianRouter
