6f523c86c4bf706f29fb44e8d42bba471808fec3
prodipkumarshil <prodipkrishna01@gmail.com>
2026-08-19 00:17:50 +0600

    added error handlers and auth

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,21 @@
 import express, { type Application, type Request, type Response } from 'express'
 import cookieParser from 'cookie-parser'
 import { notFoundHandler } from './middleware/not-found'
-import prisma from './lib/prisma'
 import authRouter from './modules/auth/auth.routes'
+import { globalErrorHandle } from './middleware/global-error'
 
 const app: Application = express()
 
 app.use(express.json())
 app.use(cookieParser())
 
-app.get('/', (req , res) => {
+app.get('/', (req, res) => {
   res.send("Server is running")
 })
 
 app.use('/api/auth', authRouter)
 
+app.use(globalErrorHandle)
 app.use(notFoundHandler)
-app.use()
 
 export default app
